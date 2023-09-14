640cf21e3e09aa66cb54b541ef6a7b59be6669bc
Albiiscoder <albanarusso.ar@gmail.com>
2023-09-13 21:36:06 -0300

    agregue condicional

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,49 @@
 
-let NombreUsario = prompt("Ingrese su nombre"); 
+let NombreUsario = prompt("Ingrese su nombre");
 const obtenerNombreCompleto = (apellido, nombre) => {
     return apellido + ", " + nombre;
 }
 
-const obtenerDetalleDeRegistro = (surName, name, email, message) => {
+const obtenerDetalleDeRegistro = (surName, name, email, phone, age, message) => {
     return (
         obtenerNombreCompleto(surName, name) +
         ", con correo electónico: " +
         email +
+        ", con telefono: " +
+        phone +
+        ", con edad: " +
+        age +
         ". Información importante del niño o niña: " +
-        message
-      );
+        message 
+    );
 }
 
 
-let respuestaUsuario = prompt("¿Quieres comenzar a completar el formulario?")
+let respuestaUsuario = prompt("¿Quieres completar un nuevo formulario?")
 while (respuestaUsuario.trim().toUpperCase() === "SI") {
 
 
     let apellidos = prompt("¿Cual es es el apellido del niño o la niña?");
     let nombres = prompt("¿Cualés son los nombres del niño o niña?")
     let correo = prompt("Ingrese un mail de contacto")
+    let telefono = prompt ("Ingrese teléfono de contacto")
+    let edad = prompt("ingrese edad del niño o la niña")
     let mensaje = prompt("Si desea ingrese información sobre el niño o niña")
 
-    alert("Tu formulario fue completado exitosamente!");
-    console.log(
-        "Elusuario" +
-        obtenerInformacionDelRegistro(apellidos, nombres, correo, telefono, edad, mensaje)
-    );
-   
+    if (apellidos != "" && nombres != "" && correo != "" && telefono != "" && edad != "") {
+        alert("Tu formulario fue completado exitosamente!");
+    } else {
+        alert("Error al cargar el formulario. Algún campo obligatorio está vacío");
+    }
+
+    console.log (
+        "El usuario " +
+        obtenerDetalleDeRegistro(apellidos, nombres, correo, telefono, edad, mensaje)
+
+    )
+    respuestaUsuario = prompt("¿Quieres completar un nuevo formulario?")    
+    
 }
 
-respuestaUsuario = prompt("¿Quieres completar un nuevo formulario?")
+
 
