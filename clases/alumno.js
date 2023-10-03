0379ec70edc5f2238f709be19ca66033c268391a
Albiiscoder <albanarusso.ar@gmail.com>
2023-10-03 18:46:30 -0300

    ultimas modificaciones

---
+++ clases/alumno.js
@@ -0,0 +1,10 @@
+class Alumno {
+    constructor (surName, name, email, phone, age, message) {
+        this.apellido= surName;
+        this.nombre= name;
+        this.email= email;
+        this.telefono= phone;
+        this.edad=age;
+        this.mensaje=message;
+    }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
-
+//Declaración del prompt
 let NombreUsario = prompt("Ingrese su nombre");
 const obtenerNombreCompleto = (apellido, nombre) => {
     return apellido + ", " + nombre;
 }
-
+//Detalle del registro de alumnos y alumnas mediante una constante
 const obtenerDetalleDeRegistro = (surName, name, email, phone, age, message) => {
     return (
         obtenerNombreCompleto(surName, name) +
@@ -17,8 +17,11 @@ const obtenerDetalleDeRegistro = (surName, name, email, phone, age, message) => 
         message 
     );
 }
+//Array
+let alumnos= [];
 
 
+//Carga manual del formulario de incripción con un ciclo while
 let respuestaUsuario = prompt("¿Quieres completar un nuevo formulario?")
 while (respuestaUsuario.trim().toUpperCase() === "SI") {
 
@@ -30,7 +33,10 @@ while (respuestaUsuario.trim().toUpperCase() === "SI") {
     let edad = prompt("ingrese edad del niño o la niña")
     let mensaje = prompt("Si desea ingrese información sobre el niño o niña")
 
+//Declaración de un booleano para posibles respuestas del formulario y objetos del array
     if (apellidos != "" && nombres != "" && correo != "" && telefono != "" && edad != "") {
+        let alumno= new Alumno(apellidos,nombres,correo, telefono, edad, mensaje);
+        alumnos.push (alumno);
         alert("Tu formulario fue completado exitosamente!");
     } else {
         alert("Error al cargar el formulario. Algún campo obligatorio está vacío");
@@ -44,6 +50,23 @@ while (respuestaUsuario.trim().toUpperCase() === "SI") {
     respuestaUsuario = prompt("¿Quieres completar un nuevo formulario?")    
     
 }
+
+for (const alumno of alumnos){
+    console.table (alumno);
+
+}
+
+//Funcion de orden superior
+const alumnos4anios= alumnos.filter((a)=>parseInt(a.edad)===4);
+console.log ("Alumnos de 4 años")
+for (const alumno of alumnos4anios){
+    console.table (alumno);
+
+}
+
+
+
+
 
 
 
